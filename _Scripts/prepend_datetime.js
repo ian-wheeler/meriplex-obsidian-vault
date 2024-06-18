3cf128e7b4226f6a7684798443264e9340d54f1c
techportrero <ianwheeler.tech@gmail.com>
2024-06-18 11:33:30 -0500

    vault backup: 2024-06-18 11:33:30

--- _Scripts/prepend_datetime.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment');
-
-function prependDatetime(filename) {
-    const datetime = moment().format('YYYYMMDDHHmmss');
-    const newFilename = `${datetime}_${filename}`;
-    return newFilename;
-}
-
-module.exports = prependDatetime;
